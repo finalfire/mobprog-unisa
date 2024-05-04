bfa18432273358061892341f8f698bc080389b36
Francesco <f.cauteruccio@gmail.com>
2024-05-04 09:34:20 +0200

    flutter snippets

---
+++ flutter/snippets/16-listview-horizontal.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const title = 'Horizontal List';
+
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(title),
+        ),
+        body: Container(
+          margin: const EdgeInsets.symmetric(vertical: 20),
+          height: 200,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[
+              Container(
+                width: 160,
+                color: Colors.red,
+              ),
+              Container(
+                width: 160,
+                color: Colors.blue,
+              ),
+              Container(
+                width: 160,
+                color: Colors.green,
+              ),
+              Container(
+                width: 160,
+                color: Colors.yellow,
+              ),
+              Container(
+                width: 160,
+                color: Colors.orange,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
