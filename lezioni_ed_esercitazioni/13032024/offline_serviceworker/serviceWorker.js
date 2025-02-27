2e234c3e4eb631790e9dc510dded5ddda6cc5d9c
finalfire <f.cauteruccio@gmail.com>
2025-02-27 16:08:06 +0100

    revamp; second

--- lezioni_ed_esercitazioni/13032024/offline_serviceworker/serviceWorker.js
+++
@@ -1,5 +0,0 @@
-console.log("Ciao, sono il service worker!");
-
-self.addEventListener("fetch", function(event) {
-    console.log("Richiesta fetch per:", event.request.url);
-});  
--- lezioni_ed_esercitazioni/18032024/serviceWorker.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener("fetch", function(event) {
-    event.respondWith(
-        fetch(event.request)
-            .catch(function(error) {
-                return new Response("Scusa, sei offline!");
-            })
-    );
-})
