68ce32aec441b449607a519dc6d3fbbd0f25a794
finalfire <f.cauteruccio@gmail.com>
2025-03-17 14:26:53 +0100

    last

---
+++ pwa/offline_serviceworker/js/app.js
@@ -0,0 +1,20 @@
+// se il mio browser SUPPORTA i ServiceWorker
+if ("serviceWorker" in navigator) { // l'oggetto navigator non è altro che il browser
+    // navigator.serviceWorker sono le API
+    // del browser per il supporto ai ServiceWorker
+    // Registrare un ServiceWorker indica associare
+    // il service worker alla mia web app
+    navigator.serviceWorker.register("/serviceWorker.js") // crea un Promise
+      .then(function(registration) {
+        console.log("Service Worker registrato con scope:", registration.scope);
+      })
+      .catch(function(error) {
+        console.log("Registrazione Service Worker fallita:", error);
+      })
+}
+
+/*
+let bottone = document.getElementById("bottone");
+bottone.addEventListener("click", (evento) => {
+    console.log("Qualcuno mi ha cliccato");
+})*/
