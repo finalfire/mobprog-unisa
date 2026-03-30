d06cf1007be596ecbded1d3aa93ffd4ebbf42532
finalfire <f.cauteruccio@gmail.com>
2026-03-30 12:34:13 +0200

    first

---
+++ react-native/navigation/stack-navigation-10.tsx
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import {useState} from 'react';
+import { View, Button, Text, TextInput } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const MainScreen = ({route, navigation}) => {
+  React.useEffect(() => {
+    if (route.params?.post) {
+      // fai qualcosa col post
+      console.log("Post scritto: " + route.params.post);
+    }
+  }, [route.params?.post]);
+
+  return (
+    <View>
+      <Button
+        title='Scrivi un post!'
+        onPress={() => navigation.navigate("Post")}
+      ></Button>
+      <Text>Il tuo post: {route.params?.post}</Text>
+    </View>
+  )
+}
+
+const PostScreen = ({route, navigation}) => {
+  const [post, setPost] = useState<string>('');
+  return (
+    <View>
+      <TextInput
+        multiline
+        placeholder='Scrivi il tuo post...'
+        value={post}
+        onChangeText={setPost}
+      ></TextInput>
+      <Button
+        title='Pubblica'
+        onPress={() => {
+          navigation.navigate({
+            name: "Main",
+            params: { post: post },
+            merge: true,
+          });
+        }}
+      ></Button>
+    </View>
+  )
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Main">
+        <Stack.Screen name="Main" component={MainScreen}/>
+        <Stack.Screen name="Post" component={PostScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ react-native/navigation/stack-navigation-3.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import { View, Button, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+function WelcomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Welcome!</Text>
+      <Button
+        title="Vai alla pagina About"
+        onPress={() => navigation.navigate("About")}
+      ></Button>
+    </View>
+  );
+}
+
+function AboutScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Some info about the MobProg course...</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{ title: "Il mio Welcome Screen" }}
+        />
+        <Stack.Screen name="About" component={AboutScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
